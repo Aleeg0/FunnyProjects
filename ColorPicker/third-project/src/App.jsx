c1667e101f7f3ac61bbaa7f23566185fc9058cd5
Aleeg0 <Sankafoxe@gmail.com>
2024-06-11 12:27:51 +0300

    add colorpicker project

---
+++ ColorPicker/third-project/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from './Header.jsx'
+import ColorPicker from './ColorPicker.jsx'
+
+function App() {
+    return(
+        <>
+            <Header></Header>
+            <ColorPicker></ColorPicker>
+        </>
+    )
+}
+
+export default App
---
+++ ColorPicker/third-project/src/Header.jsx
@@ -0,0 +1,11 @@
+import './Header.css'
+
+function Header(){
+    return(
+        <div className='headerInfo'>
+            <h3>Welcome to Color Picker</h3>
+        </div>
+    )
+}
+
+export default Header;

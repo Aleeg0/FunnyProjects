7b8cfd87ec066c9577ba997b269b0b0307ee766b
Aleeg0 <Sankafoxe@gmail.com>
2024-07-02 17:46:30 +0300

    update stopwatch

---
+++ counter/src/Components/StopWatch/Stopwatch.tsx
@@ -0,0 +1,67 @@
+import React, {useEffect} from 'react';
+
+const Stopwatch = () => {
+    const [time,setTime] = React.useState<number>(0);
+    const [isPaused,setIsPaused] = React.useState<boolean>(true);
+    const timerId = React.useRef< ReturnType<typeof setInterval>| null>(null);
+    const startTime = React.useRef<number>(0);
+
+    useEffect(() => {
+        if (!isPaused) {
+            console.log('effect func')
+            timerId.current = setInterval(() => {
+                setTime(Date.now() - startTime.current);
+            }, 10);
+        }
+        return () => {
+            console.log('effect func ret')
+            clearInterval(timerId.current!);
+        }
+    }, [isPaused]);
+
+    const start = () => {
+        setIsPaused(false);
+        startTime.current = Date.now() - time;
+    }
+
+    const pause = () => {
+        setIsPaused(true);
+    }
+
+    const ch = () => {
+        if (isPaused){
+            startTime.current = Date.now() - time;
+        }
+        setIsPaused(!isPaused);
+    }
+
+    const resetStopwatch = () => {
+        startTime.current = Date.now();
+        setTime(t => t = 0);
+    }
+
+    const hours = Math.floor(time / 3600000);
+    const minutes = Math.floor(time / 60000 % 60);
+    const seconds = Math.floor(time / 1000 % 60);
+    const milliseconds = time % 100;
+
+    return (
+        <div className="stopwatch">
+            <h1>
+                {hours.toString().padStart(2,"0")}:
+                {minutes.toString().padStart(2,"0")}:
+                {seconds.toString().padStart(2,"0")}:
+                {milliseconds.toString().padStart(2,"0")}
+            </h1>
+            <div className="controls">
+                <button className={isPaused ? "controls__pause":"controls__resume"} onClick={ch}>
+                    {isPaused ? `▷` : `||`}
+                </button>
+                <button className="controls__reset" onClick={resetStopwatch}>↻</button>
+                <button className="controls__cancel" onClick={pause}>✖</button>
+            </div>
+        </div>
+    );
+};
+
+export default Stopwatch;

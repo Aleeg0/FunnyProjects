b4651ad378eb8f48526d59c67fe9a8016d76842f
Aleeg0 <Sankafoxe@gmail.com>
2024-07-03 12:30:32 +0300

    add users component

---
+++ counter/src/Components/Users/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Dummy from "./Dummy";
+
+const Users = () => {
+    return (
+        <div className="usersBox">
+            <div className="searcher">
+                <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z"/>
+                </svg>
+                <input type="text" placeholder="Search user..."/>
+            </div>
+            <div className="usersList">
+                <Dummy/>
+                <Dummy/>
+                <Dummy/>
+            </div>
+        </div>
+    );
+};
+
+export default Users;

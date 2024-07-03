5e764eb0a16d19e14af88533ca75d0a7ed55dd70
Aleeg0 <Sankafoxe@gmail.com>
2024-07-03 16:00:07 +0300

    add fetching data to project

--- counter/src/App.tsx
+++ counter/src/App.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Users from "./Components/Users";
+import {IUser} from "./Models/user";
 
 function App() {
+
+    const [usersList, setUsersList] = useState<IUser[]>([]);
+    const [searchValue, setSearchValue] = useState<string>("");
+
+    useEffect(() => {
+        fetch("https://reqres.in/api/users")
+        .then(data => data.json())
+        .then(res => setUsersList(res.data));
+    }, []);
+
+    const onChangeSearchValue = (event:React.ChangeEvent<HTMLInputElement>) => {
+        setSearchValue(event.target!.value);
+    }
+
     return (
         <div className="app">
-            <Users isLoading={false}/>
+            <Users users={usersList} isLoading={false} searchValue={searchValue} onChangeSearchValue={onChangeSearchValue}/>
         </div>
     );
 }

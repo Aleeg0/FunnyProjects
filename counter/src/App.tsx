54aaec0edf47d7014944a2c178dbf6126ed4ec4f
Aleeg0 <Sankafoxe@gmail.com>
2024-07-04 22:16:01 +0300

    add effects to fetching data from API

--- counter/src/App.tsx
+++ counter/src/App.tsx
@@ -1,11 +1,38 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import Convertor from "./Components/convertBlock";
+import {CurrencyApiClient, LatestResponse} from "./API/currencyAPI/CurrencyAPI";
+
+const API_KEY = "cur_live_lBfeZrnYZm14XUzmHN1pXglWaLAOFoYMZnyzMxnk";
 
 function App() {
 
+    const rates = useRef<LatestResponse|null>(null);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+
+    const currencyAPI = new CurrencyApiClient(API_KEY);
+
+    useEffect(() => {
+        currencyAPI.getLatestRates()
+        .then(resp => {
+            rates.current = resp;
+            setIsLoading(false);
+            console.log(resp);
+        })
+        .catch((er) => {
+            console.log(er);
+        })
+    }, []);
+
     return(
         <div className="app">
-            <Convertor/>
+            {
+                isLoading ?
+                    <p>Loading...</p>
+                    :
+                    <Convertor
+                        rates={rates.current!}
+                    />
+            }
         </div>
     );
 }

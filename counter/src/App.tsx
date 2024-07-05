24c526c03f5fffeca67c2043f4772e262ecc98fe
Aleeg0 <Sankafoxe@gmail.com>
2024-07-05 19:01:29 +0300

    init project

--- counter/src/App.tsx
+++ counter/src/App.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import Counter from "./Components/counter/counter";
 
 function App() {
     return (
         <div className="app">
-            <Counter/>
         </div>
     );
 }
---
+++ counter/src/Components/GalleryCollection/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GalleryCollection = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default GalleryCollection;
